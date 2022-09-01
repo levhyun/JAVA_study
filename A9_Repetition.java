ffddc61c400c19d040ba053ca4f44db6f641394a
levhyun <g88080997@gmail.com>
2022-09-01 11:44:08 +0900

    Update A9_Repetition.java

--- A9_Repetition.java
+++ A9_Repetition.java
@@ -2,6 +2,18 @@
 
 public class A9_Repetition {
     public static void main(String[] args) {
+        // while
+        /*
+            구조
 
+            while (condition) {
+
+            }
+        */
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
     }
 }
