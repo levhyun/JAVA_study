3cb944d400dbc8f78f23883b80116241162c0479
levhyun <g88080997@gmail.com>
2022-09-23 15:56:31 +0900

    2

--- C7_Arraylist.java
+++ C7_Arraylist.java
@@ -3,11 +3,14 @@
 import java.util.ArrayList; // 배열 라이브러리
 import java.util.Collections; // 정렬 라이브러리
 import java.util.LinkedList; // Linkedlist (=Arraylist)
+import java.util.HashMap; // HashMap 라이브러리
+import java.util.HashSet; // HashSet 라이브러리
+import java.util.Iterator; // 배열 반복자 라이브러리
 
 
 public class C7_Arraylist {
     public static void main(String[] args) {
-        ArrayList<String> arr = new ArrayList<String>(); // 벼열 선언
+        ArrayList<String> arr = new ArrayList<>(); // 벼열 선언
         arr.add("1"); // 배열에 값 추가
         arr.add("2");
         arr.add("3");
@@ -19,7 +22,7 @@ public class C7_Arraylist {
         arr.remove(3); // 배열에서 index번호의 값 삭제
         arr.clear(); // 배열 초기화
 
-        ArrayList<String> test = new ArrayList<String>();
+        ArrayList<String> test = new ArrayList<>();
         test.add("1");
         test.add("2");
         test.add("3");
@@ -28,7 +31,7 @@ public class C7_Arraylist {
 
 
         // 정수 배열
-        ArrayList<Integer> num = new ArrayList<Integer>();
+        ArrayList<Integer> num = new ArrayList<>();
         num.add(1);
         num.add(3);
         num.add(2);
@@ -41,11 +44,48 @@ public class C7_Arraylist {
         System.out.println(num);
 
 
-        LinkedList<String> arr2 = new LinkedList<String>(); // 링크드리스트 선언
+        LinkedList<String> arr2 = new LinkedList<>(); // 링크드리스트 선언
         arr2.add("1");
         arr2.add("2");
         arr2.add("3");
         arr2.add("4");
         System.out.println(arr2);
+
+        // HashMap
+        HashMap<String, Integer> hm = new HashMap<>();
+        hm.put("A", 1);
+        hm.put("B", 2);
+        hm.put("C", 3);
+        hm.put("D", 4);
+        for (String i : hm.keySet()) {
+            System.out.println("name : "+ i +"\n(" + i + ")number : " + hm.get(i));
+        }
+
+        // HashSet
+        HashSet<Integer> numbers = new HashSet<Integer>();
+        numbers.add(2);
+        numbers.add(4);
+        numbers.add(8);
+        numbers.add(16);
+        for (int i = 1; i <= 20; i++) {
+            if (numbers.contains(i)) { // 배열변수.contains(값1) <= 배열에서 값1이 있는지 확인
+                System.out.println(i);
+            }
+            else {
+                System.out.println("ERROR");
+            }
+        }
+
+        // iterator
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        Iterator<String> it = cars.iterator();
+        System.out.printf("\n");
+        for (int i = 0; i < 4; i++) {
+            System.out.println("["+it.next()+"]");
+        }
     }
 }
